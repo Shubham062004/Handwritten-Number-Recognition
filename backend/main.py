aeea1af82cf686abb6942d76f2c1622d80191360
Shubham <shubhamkumar081908@gmail.com>
2026-01-02 17:32:37 +0530

    Single digit prediction working

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from PIL import Image
 import numpy as np
 import io
+import tensorflow as tf
 
 app = FastAPI()
 
@@ -13,23 +14,36 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Load CNN model once (VERY IMPORTANT)
+model = tf.keras.models.load_model("cnn_model.h5")
+
 @app.get("/")
 def home():
     return {"message": "FastAPI is working"}
 
 @app.post("/upload")
 async def upload_image(file: UploadFile = File(...)):
-    # Read image bytes
+    # Read image
     image_bytes = await file.read()
-
-    # Convert bytes to PIL Image
     image = Image.open(io.BytesIO(image_bytes)).convert("L")
 
-    # Convert image to numpy array
+    # Resize to 28x28
+    image = image.resize((28, 28))
+
+    # Convert to numpy
     image_array = np.array(image)
 
+    # Normalize
+    image_array = image_array / 255.0
+
+    # Reshape for CNN
+    image_array = image_array.reshape(1, 28, 28, 1)
+
+    # Predict digit
+    prediction = model.predict(image_array)
+    predicted_digit = int(np.argmax(prediction))
+
     return {
-        "filename": file.filename,
-        "image_shape": image_array.shape,
-        "message": "Image processed successfully"
+        "predicted_digit": predicted_digit,
+        "message": "Prediction successful"
     }
